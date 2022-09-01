a8d0dba36e73813567b47f9b1f00ba1be3bf0706
Stefanos <stefanosu422@gmail.com>
2022-08-31 20:49:03 -0400

    Update ProgramController.cs

--- Controllers/ProgramController.cs
+++ Controllers/ProgramController.cs
@@ -7,5 +7,10 @@ namespace TicTacToe.Controllers;
 class Program
 {
     private readonly ILogger<ProgramController> _logger;
+    
+    public ProgramController(ILogger<ProgramController> logger)
+    {
+        _logger = logger;
+    }
 
 }
